e609410c14ce01c89ea12a98faebabc93f2b7a96
hirano-vm4 <hirano.vm4@gmail.com>
2024-02-24 22:43:35 +0900

    [リファクタリング]searchスコープをlambdaから->(アロー関数)表記に変更統一

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -9,9 +9,7 @@ class Blog < ApplicationRecord
 
   scope :published, -> { where('secret = FALSE') }
 
-  scope :search, lambda { |term|
-    where('title LIKE :term OR content LIKE :term', term: "%#{term}%")
-  }
+  scope :search, ->(term) { where('title LIKE :term OR content LIKE :term', term: "%#{term}%") }
 
   scope :default_order, -> { order(id: :desc) }
 
